4a165ea359a1ba0ed916d964456489297f8415df
Kevin Kuo <kvkkuo@gmail.com>
2018-07-27 00:16:23 -0400

    no more holding down spacebar

--- assets/js/rhythm.js
+++ assets/js/rhythm.js
@@ -5,17 +5,21 @@ var interval = 0;
 var beat = 0;
 var beating = 0;
 var metronome;
+var keydown = 0;
 var NOTE_TYPES = [4, 3, 2, 1.5, 1, 0.75, 0.5, 0.25, 0.33, 0.66];
 
 $(document).ready(function () {
 	$("#bpm").click(startBPM);
 	$("#notate").click(startNotation);
+	$(window).keyup(function(event) {
+		keydown = 0;
+	})
 });
 function createRipple() {
 	var ripple = $("<div class='ripple'></div>").appendTo("body");
 	setTimeout(function(){
 		ripple.remove();
-	}, 3500); 
+	}, 2500); 
 }
 function startBPM() {
 	if (recording == 0) {
@@ -27,17 +31,20 @@ function startBPM() {
 		var lastTap = 0;
 		var intervalSum = 0; // intervalSum/totalTaps = averageInterval
 		var totalTaps = 0;
-		$(window).keypress(function( event ) {
-			if (event.which == 32) { // Spacebar
-				createRipple();
-				totalTaps += 1;
-				if (totalTaps > 1) {
-					intervalSum += event.timeStamp - lastTap;
-					bpm = Math.round(60000/(intervalSum/totalTaps));
-					interval = Math.round(60000/bpm);
-					$("#bpm").html("BPM: " + bpm + "<br>Click to lock BPM."); // 60000ms in 1 minute
+		$(window).keydown(function( event ) {
+			if (keydown == 0) {
+				keydown = 1;
+				if (event.which == 32) { // Spacebar
+					createRipple();
+					totalTaps += 1;
+					if (totalTaps > 1) {
+						intervalSum += event.timeStamp - lastTap;
+						bpm = Math.round(60000/(intervalSum/totalTaps));
+						interval = Math.round(60000/bpm);
+						$("#bpm").html("BPM: " + bpm + "<br>Click to lock BPM."); // 60000ms in 1 minute
+					}
+					lastTap = event.timeStamp;
 				}
-				lastTap = event.timeStamp;
 			}
 		});
 	}
@@ -46,7 +53,7 @@ function startBPM() {
 function stopRecording() {
 	if (recording == 1) {
 		recording = 0;
-		$(window).unbind("keypress");
+		$(window).unbind("keydown");
 		displayBeat();
 		$("#bpm").html("BPM: " + bpm + "<br>Click to record new BPM.");
 		$("#bpm").unbind("click").click(startBPM);
@@ -76,14 +83,14 @@ function startNotation() {
 			recording = 1;
 			taps = [];
 			$("#notate").text("Tap spacebar to begin notating.");
-			$(window).keypress(function( event ) {
+			$(window).keydown(function( event ) {
 				if (event.which == 32) { // Spacebar
 					createRipple();
 					if (taps.length == 0) {
 						$("#notate").text("Notating for 8 measures...");
 						setTimeout(function(){
 							recording = 0;
-							$(window).unbind("keypress");
+							$(window).unbind("keydown");
 							$("#notate").html("Done! Data in console.<br>Click to record new rhythm.");
 							parseTaps();
 						}, interval*32);
