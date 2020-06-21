3ca70a315d231b480c030a0ebe0189ccee80c6a0
Kevin Kuo <kvkkuo@gmail.com>
2020-06-21 00:34:53 -0400

    Going to do blog posts through Google Drive, +project images

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,18 @@
-// HTML Anchor Smooth Scroll
-$(document).on('click', 'a[href^="#"]', function (event) {
-    event.preventDefault();
+$( document ).ready(function() {
+    // HTML Anchor Smooth Scroll
+	$(document).on('click', 'a[href^="#"]', function (event) {
+		event.preventDefault();
 
-    $('html, body').animate({
-        scrollTop: $($.attr(this, 'href')).offset().top - 64
-    }, 500, "swing");
+		$('html, body').animate({
+			scrollTop: $($.attr(this, 'href')).offset().top - 64
+		}, 500, "swing");
+	});
+	
+	$("a > i").on("mouseenter", function(event) {
+		$("#icon-desc").text($(event.target).attr("hovertext"));
+	});
+	
+	$("a > i").on("mouseout", function(event) {
+		$("#icon-desc").text("");
+	});
 });
