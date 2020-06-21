c4b459163f87c8069ae9f7308b2713b4b0fb2ac4
Kevin Kuo <kvkkuo@gmail.com>
2020-06-21 12:15:24 -0400

    Blog posts depend on external repo now!

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,36 @@
+var BLOG_REPO = "https://raw.githubusercontent.com/imkevinkuo/blog/master/";
+var converter = new showdown.Converter()
+
+var LATEST_POST = 0;
+var blog = null;
+
+function insertBlogPost(i){
+	$.ajax({
+		type: "GET",
+		url: BLOG_REPO + i + ".md",
+		dataType: "text",
+		success: function(data) {
+			html = converter.makeHtml(data);
+			var blogpost = document.createElement('div');
+			blogpost.classList.add("blogpost");
+			blogpost.innerHTML = html;
+			blog.appendChild(blogpost);
+		}
+	});
+}
+
+function getLatestPost() {
+	$.ajax({
+		type: "GET",
+		url: BLOG_REPO + "latest_post.txt",
+		async: false,
+		dataType: "text",
+		success: function(data) {
+			LATEST_POST = parseInt(data);
+		}		
+	});
+}	
+
 $( document ).ready(function() {
     // HTML Anchor Smooth Scroll
 	$(document).on('click', 'a[href^="#"]', function (event) {
@@ -8,6 +41,7 @@ $( document ).ready(function() {
 		}, 500, "swing");
 	});
 	
+	// Icons below profile pic
 	$("a > i").on("mouseenter", function(event) {
 		$("#icon-desc").text($(event.target).attr("hovertext"));
 	});
@@ -15,4 +49,10 @@ $( document ).ready(function() {
 	$("a > i").on("mouseout", function(event) {
 		$("#icon-desc").text("");
 	});
+	
+	// Create blog posts
+	blog = document.getElementById('blog');
+	for (var i = LATEST_POST; i >= 0; i--) {
+		insertBlogPost(i);
+	}
 });
